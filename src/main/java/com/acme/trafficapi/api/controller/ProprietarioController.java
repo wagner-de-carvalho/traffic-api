10a42c945b25728f272790a1347dea0cd1603934
mindlab <wagncarv@outlook.com>
2023-10-14 19:33:21 -0300

    Implementando endpoint de inclusão de recurso

--- src/main/java/com/acme/trafficapi/api/controller/ProprietarioController.java
+++ src/main/java/com/acme/trafficapi/api/controller/ProprietarioController.java
@@ -2,10 +2,15 @@ package com.acme.trafficapi.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.acme.trafficapi.domain.model.Proprietario;
 import com.acme.trafficapi.domain.repository.ProprietarioRepository;
 
@@ -21,5 +26,18 @@ public class ProprietarioController {
     @GetMapping
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 }
--- src/main/java/com/acme/trafficapi/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/acme/trafficapi/domain/repository/ProprietarioRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.acme.trafficapi.domain.model.Proprietario;
+import java.util.List;
 
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
-
+    List<Proprietario> findByNomeContaining(String nome);
 }
