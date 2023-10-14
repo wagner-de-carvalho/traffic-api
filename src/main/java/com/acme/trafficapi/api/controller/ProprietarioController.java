3737808b6e01239011a941d17922b7b96159e96d
mindlab <wagncarv@outlook.com>
2023-10-14 17:06:06 -0300

    Consulta sql usando EntityManager

--- src/main/java/com/acme/trafficapi/api/controller/ProprietarioController.java
+++ src/main/java/com/acme/trafficapi/api/controller/ProprietarioController.java
@@ -1,5 +1,6 @@
 package com.acme.trafficapi.api.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,14 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.acme.trafficapi.domain.model.Proprietario;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 @RequestMapping("/proprietarios")
 public class ProprietarioController {
+    @PersistenceContext
+    private EntityManager manager;
 
     @GetMapping
     public List<Proprietario> listar() {
-        var p1 = new Proprietario(1L, "Carles", "carles@mail.com", "333356478");
-        var p2 = new Proprietario(2L, "Penelope", "penelope@mail.com", "227745691");
-        return Arrays.asList(p1, p2);
+        return manager
+                .createQuery("from Proprietario", Proprietario.class)
+                .getResultList();
     }
 }
--- src/main/java/com/acme/trafficapi/domain/model/Proprietario.java
+++ src/main/java/com/acme/trafficapi/domain/model/Proprietario.java
@@ -1,13 +1,28 @@
 package com.acme.trafficapi.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Proprietario {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 }
