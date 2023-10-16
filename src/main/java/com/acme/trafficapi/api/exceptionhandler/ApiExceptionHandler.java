489db44497e16b3220e86d5f63db35b2793c551f
mindlab <wagncarv@outlook.com>
2023-10-15 23:25:01 -0300

    Adicionando campos customizados no ProblemDetail

--- src/main/java/com/acme/trafficapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/acme/trafficapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.acme.trafficapi.api.exceptionhandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,6 +29,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos");
         problemDetail.setType(URI.create("https://acmetraffic.com/erros/campos-invalidos"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        objectError -> ((FieldError) objectError).getField(),
+                        DefaultMessageSourceResolvable::getDefaultMessage));
+
+        problemDetail.setProperty("fields", fields);
 
         return handleExceptionInternal(ex, problemDetail, headers, status, request);
     }
